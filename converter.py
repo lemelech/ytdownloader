d69aca21a2a3656b9a65ff79eb5034fdb4ab0b59
Elimelech Schreiber <elimelech.schreiber@scrdairy.com>
2023-02-21 18:04:05 +0200

    working poc

---
+++ converter.py
@@ -0,0 +1,22 @@
+import subprocess
+from pathlib import Path
+
+
+def convert2mp3(file_path, quality=5):
+    file_path = str(file_path)
+    print('starting to convert: {file_path}...')
+    process = subprocess.Popen(['ffmpeg', '-i', file_path, '-q:a', str(quality), '-map', 'a', file_path + '.mp3'],
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    print(stdout, stderr)
+
+
+def convert_all_to_mp3(dir_path, pat='*.mp4'):
+    dir_path = Path(dir_path)
+    for file_path in dir_path.glob(pat):
+        convert2mp3(str(file_path))
+
+
+if __name__ == "__main__":
+    convert_all_to_mp3('.')
