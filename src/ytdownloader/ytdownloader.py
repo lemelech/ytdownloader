8ee1c715ee383ac18f9747cb9c1fca150b196971
Elimelech <elimelech.schreiber@merck.com>
2024-05-07 03:23:49 +0300

    yt_dlp transitioned

--- src/ytdownloader/ytdownloader.py
+++ src/ytdownloader/ytdownloader.py
@@ -1,22 +1,24 @@
-from pytube import YouTube
 from .ClipboardListener import ClipboardWatcher
 from .converter import convert2mp3
 from pathlib import Path
 import time
+from yt_dlp import YoutubeDL
 
-from pytube.innertube import _default_clients
-_default_clients["ANDROID_MUSIC"] = _default_clients["ANDROID"]
-_default_clients["ANDROID"]["context"]["client"]["clientVersion"] = "19.08.35"
-_default_clients["IOS"]["context"]["client"]["clientVersion"] = "19.08.35"
-_default_clients["ANDROID_EMBED"]["context"]["client"]["clientVersion"] = "19.08.35"
-_default_clients["IOS_EMBED"]["context"]["client"]["clientVersion"] = "19.08.35"
-_default_clients["IOS_MUSIC"]["context"]["client"]["clientVersion"] = "6.41"
-_default_clients["ANDROID_MUSIC"] = _default_clients["ANDROID_CREATOR"]
 
 # where to save
 SAVE_PATH = Path(__file__).parent / "downloads"
 LOG_PATH = Path(__file__).parent / "downloads.log"
 
+
+ydl_opts = {
+            'format': 'm4a/bestaudio/best',
+            # ℹ️ See help(yt_dlp.postprocessor) for a list of available Postprocessors and their arguments
+            'postprocessors': [{  # Extract audio using ffmpeg
+                                'key': 'FFmpegExtractAudio',
+                                'preferredcodec': 'mp3',}#'m4a',}
+                               ],
+            'nocheckcertificate': True
+            }
 def is_youtube_url(url:str):
     return any([pat in url for pat in ["youtube", "youtu.be"]]) and (url.lower().startswith("http://") or
                                                                      url.lower().startswith("https://") or
@@ -25,24 +27,14 @@ def is_youtube_url(url:str):
 
 def ytdownloader(url):
     try:
-        yt = YouTube(url)
-    except Exception as e:
-        print("Connection Error")  # to handle exception
-        print(e)
-
-    try:
-        # for debugging:
-        # for stream in yt.streams.filter(file_extension='mp4').order_by('abr').desc():
-        #     print(stream)
-
-        stream = yt.streams.filter(file_extension='mp4').order_by('abr').desc()[1]
-        print(f'Downloading {stream}')
-        file_path = stream.download(SAVE_PATH)
+        print(f'Downloading {url}')
+        with YoutubeDL(ydl_opts) as ydl:
+            ydl.download(url)
 
         with open(LOG_PATH, 'a') as f:
-            f.write(f'{url} : {file_path}\n')
-        print(f'Converting {file_path}')
-        convert2mp3(file_path)
+            f.write(f'{url} \n')
+        # print(f'Converting {file_path}')
+        # convert2mp3(file_path)
     except Exception as e:
         print(e)
 
